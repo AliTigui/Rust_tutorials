e782cba049ca3e6e6972e51553b9584505b07742
AliTigui <alitighiouart2001@gmail.com>
2024-02-04 16:06:34 +0100

    added code for ownership

---
+++ tutorial3/main.rs
@@ -0,0 +1,46 @@
+fn main(){
+    /*
+     * scope is where variable is valide to use when variable get out of scope it get droped 
+     * integer char literal-string array boolean and tupple are craeted in the stack
+     * complex type like String use head and those type of variables apply ownership rules
+     * when we assign variable that are stored in heap to another one the new one become owner and old become invalide to use
+     * passing those complex variables to function make function parameter take ownership of them
+     * function don't take ownership of simple variables like integer boolean ...
+     * to fix ownership problems we use reference
+     * reference is like making pointer to variables in the heap
+     * Reference rule :
+       - At any given time, you can have either one mutable reference or any number of immutable references.
+       - References must always be valid.
+     * 
+     * 
+     */
+    //===================== Scope ===================== 
+    let a=4;
+    let b=a;
+    {
+        let c=4;
+        println!("a {} and b {} c {} ",a,b,c);
+    }//c here is out of scope
+    println!("both a {} and b {} still working",a,b);
+    //====================== ownership ================
+    let s1:String=String::from("hello");
+    let s2=s1; //s1 get new owner s2 if we try use s1 after this line we get error
+    //println!("{}",s1);
+    println!("{}",s2);
+    let s3=s2.clone();
+    func1(s2);// if you use s2 after this function you get error
+    //println!("{}",s2);
+    func2(a);// you can use a after this function
+    println!("{}",a);
+    func3(&s3);// we can still be able to use s3 after function call
+    println!("s3 still valide /{}/",s3)
+}
+fn func1(p:String){
+    println!("this function take ownership of p /{}/ you can't use it more",p);
+}
+fn func2(p:i32){
+    println!("this function just made copy of p /{}/ you can use after the function",p);
+}
+fn func3(p:&String){
+    println!("this value take only reference of /{}/ it don't take ownership",*(p))
+}
---
+++ tutorial3/readme.md
@@ -0,0 +1,5 @@
+** TURORIAL 
+* In this tutorial we will see :
+1-: Ownership / Reference
+2-: String
+3-: Variables Scope
