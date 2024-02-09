dbaf13c1821dd01b94aa0c4a2e5fd834a879f385
AliTigui <alitighiouart2001@gmail.com>
2024-02-09 23:02:33 +0100

    aaaa

--- tutorial7/main.rs
+++ tutorial7/main.rs
@@ -4,7 +4,7 @@
  * strings
  * hash map
  * */
-
+ use std::collections::HashMap;
  fn main(){
 
     // create empty vector
@@ -49,5 +49,31 @@
     println!("{me}");
     let ss = format!("{me}-{s}-{name}"); // like println but print to string
     println!("{ss}");
-    
+    // hash map
+    let mut scores = HashMap::new();//create hashmap
+
+    scores.insert(String::from("Blue"), 10);// add value
+    scores.insert(String::from("Yellow"), 50);
+    let va=scores.get("Blue").copied().unwrap_or(0);
+    println!("{va}");
+    // iterate over hashmap
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+    scores.entry(String::from("Blue")).or_insert(50);// entry return reference; to value off added key
+    //if key exist entery will do nothing
+
+    //exemple use of hash map and updaating value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    for (key, value) in &map{
+        println!("{key}: {value}");
+    }
  }
